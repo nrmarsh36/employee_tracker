b8d17dfc9f02d14e8bbf3e948eb5357258cec93d
nrmarsh36 <nrmarsh36@gmail.com>
2020-07-31 09:00:35 -0500

    set up initial prompts, started skeleton of functions

--- server.js
+++ server.js
@@ -14,3 +14,27 @@ connection.connect(function(err) {
   if (err) throw err;
   start();
 });
+
+// Add departments, roles, employees
+// View departments, roles, employees
+// Update employee roles
+
+function start() {
+    inquirer
+    .prompt({
+        name:"action",
+        type:"rawlist",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all employees by department",
+            "View all employees by manager",
+            "Add an employee",
+            "Remove an employee",
+            "Update an employee's role",
+            "Update an employee's manager"
+        ]
+
+
+    })
+}
